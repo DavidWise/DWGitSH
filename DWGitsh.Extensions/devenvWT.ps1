292bb0bdcf38dbcad9ff7e2654192f726acc24b5
David Wise <github@darksidestory.com>
2022-12-11 14:15:09 -0500

    Fixed WT launch to hide interim window

--- DWGitsh.Extensions/devenvWT.ps1
+++ DWGitsh.Extensions/devenvWT.ps1
@@ -1,8 +1,6 @@
-﻿# launch the 
+﻿# launches the Windows Terminal using the profile configured
 
-# Import-Module .\DWGitsh.Extensions.dll -force;
-
-$PSCommand = "Import-Module .\DWGitsh.Extensions.dll -force; cd PSScripts; .\Start-GitShell.ps1;"
+$tabColor = "#0A9999"
 
 $curFolder = get-item .
 $curDir = $curFolder.Fullname
@@ -11,32 +9,9 @@ $rootFolder = get-item $env:VSProjectDir
 
 $devenvPath = "$($env:VSProjectDir)devenv.ps1"
 
-#write-host "You will need to manually load the dev environment in Windows Terminal to begin working" -ForegroundColor Yellow
-#write-host "as Windows Terminal does not yet support passing arguments to powershell commands as part of the startup" -ForegroundColor Yellow
-
-#Write-Host ""
-#Write-Host "Note: As of now, debugging is not possible from a Windows Terminal session" -ForegroundColor Red
-#Write-Host ""
-
-#Write-Host ""
-#write-host "Type: (or paste from clipboard)" -ForegroundColor Cyan
-
-#Write-Host ""
-
-#Write-Host "Working : $curDir" -ForegroundColor Green
-#Write-Host "script  : $devenvPath" -ForegroundColor Green
-#Set-Clipboard $devenvPath
-
-#Write-Host ""
-
-#Write-Host "Press [Enter] to launch Windows Terminal... "
-#Read-Host
-
-$wtArgs = @("WT", "--title `"DWGitsh Dev Env`"", "-d `"$curDir`"", "powershell.exe")
-
 $windowTitle = "DWGitsh Dev Env - $($curFolder.Name)"
 
-$cmdArgs = "new-tab --title `"$($windowTitle)`" --tabColor '#0A9999' -d `"$curDir`""
+$cmdArgs = "new-tab --title `"$($windowTitle)`" --tabColor `"$tabColor`" -d `"$curDir`""
 $cmdArgs +=" pwsh.exe -noexit -File `"$devenvPath`" "
 
 $startExe = New-Object System.Diagnostics.ProcessStartInfo
