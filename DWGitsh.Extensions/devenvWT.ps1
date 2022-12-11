a264712278268d5d1f85c5ce2c37f5448c125c18
David Wise <github@darksidestory.com>
2022-12-11 13:59:47 -0500

    Fixed Launch and debug profiles. Changed to use variables in launchJSON

--- DWGitsh.Extensions/devenvWT.ps1
+++ DWGitsh.Extensions/devenvWT.ps1
@@ -3,38 +3,50 @@
 # Import-Module .\DWGitsh.Extensions.dll -force;
 
 $PSCommand = "Import-Module .\DWGitsh.Extensions.dll -force; cd PSScripts; .\Start-GitShell.ps1;"
-$curDir = (Get-location).Path
 
-$devenvPath = "..\..\..\devenv.ps1"
+$curFolder = get-item .
+$curDir = $curFolder.Fullname
 
-write-host "You will need to manually load the dev environment in Windows Terminal to begin working" -ForegroundColor Yellow
-write-host "as Windows Terminal does not yet support passing arguments to powershell commands as part of the startup" -ForegroundColor Yellow
+$rootFolder = get-item $env:VSProjectDir
 
-Write-Host ""
-Write-Host "Note: As of now, debugging is not possible from a Windows Terminal session" -ForegroundColor Red
-Write-Host ""
+$devenvPath = "$($env:VSProjectDir)devenv.ps1"
 
-Write-Host ""
-write-host "Type: (or paste from clipboard)" -ForegroundColor Cyan
+#write-host "You will need to manually load the dev environment in Windows Terminal to begin working" -ForegroundColor Yellow
+#write-host "as Windows Terminal does not yet support passing arguments to powershell commands as part of the startup" -ForegroundColor Yellow
 
-Write-Host ""
+#Write-Host ""
+#Write-Host "Note: As of now, debugging is not possible from a Windows Terminal session" -ForegroundColor Red
+#Write-Host ""
 
-Write-Host "$devenvPath" -ForegroundColor Green
-Set-Clipboard $devenvPath
+#Write-Host ""
+#write-host "Type: (or paste from clipboard)" -ForegroundColor Cyan
 
-Write-Host ""
+#Write-Host ""
 
-Write-Host "Press [Enter] to launch Windows Terminal... "
-Read-Host
+#Write-Host "Working : $curDir" -ForegroundColor Green
+#Write-Host "script  : $devenvPath" -ForegroundColor Green
+#Set-Clipboard $devenvPath
+
+#Write-Host ""
+
+#Write-Host "Press [Enter] to launch Windows Terminal... "
+#Read-Host
 
 $wtArgs = @("WT", "--title `"DWGitsh Dev Env`"", "-d `"$curDir`"", "powershell.exe")
 
+$windowTitle = "DWGitsh Dev Env - $($curFolder.Name)"
+
+$cmdArgs = "new-tab --title `"$($windowTitle)`" --tabColor '#0A9999' -d `"$curDir`""
+$cmdArgs +=" pwsh.exe -noexit -File `"$devenvPath`" "
 
 $startExe = New-Object System.Diagnostics.ProcessStartInfo
-$startExe.Arguments = "--title `"DWGitsh Dev Env`" -d `"$curDir`" `"powershell.exe`""
-$startExe.WorkingDirectory = $curDir
+$startExe.Arguments = $cmdArgs
+$startExe.WorkingDirectory = "$curDir"
 $startExe.FileName = "WT"
-
 
 $proc = [System.Diagnostics.Process]::Start($startExe)
 
+Start-sleep -seconds 2
+
+$runningProc = get-process | where {$_.MainWindowTitle -eq $windowTitle} 
+$runningProc.WaitForExit()
